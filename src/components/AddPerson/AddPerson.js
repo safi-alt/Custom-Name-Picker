3daab26485f2d91e345a0b39894d3885eca71451
safi <safiazim8@gmail.com>
2020-07-11 00:12:51 +0500

    Combining local UI state and Redux

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./AddPerson.css";
 
-const addPerson = (props) => (
-  <div className="AddPerson">
-    <button onClick={props.personAdded}>Add Person</button>
-  </div>
-);
+class AddPerson extends Component {
+  state = {
+    name: "",
+    age: "",
+  };
 
-export default addPerson;
+  nameChangeHandler = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  ageChangeHandler = (event) => {
+    this.setState({ age: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className="AddPerson">
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={this.nameChangeHandler}
+          value={this.state.name}
+        />
+        <input
+          type="Number"
+          placeholder="Age"
+          onChange={this.ageChangeHandler}
+          value={this.state.age}
+        />
+        <button
+          onClick={() =>
+            this.props.personAdded(this.state.name, this.state.age)
+          }
+        >
+          Add Person
+        </button>
+      </div>
+    );
+  }
+}
+
+export default AddPerson;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,8 +9,8 @@ const reducer = (state = initialState, action) => {
     case actionTypes.ADD_PERSON:
       const newPerson = {
         id: Math.random(), // not really unique but good enough here!
-        name: "Max",
-        age: Math.floor(Math.random() * 40),
+        name: action.personData.name,
+        age: action.personData.age,
       };
       return {
         ...state,
