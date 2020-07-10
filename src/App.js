58d70b6d54b9a433461afa48ab1b8ee51871bb50
safi <safiazim8@gmail.com>
2020-07-11 00:35:54 +0500

    Adjusted CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Persons from './containers/Persons';
+import Persons from "./containers/Persons";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ol>
-          <li>Turn this app into one which does NOT use local state (in components) but instead uses Redux</li>
-        </ol>
+        <h1
+          style={{
+            textAlign: "center",
+            color: "white",
+            boxSizing: "border-box",
+            boxShadow: "0 2px 2px #ccc",
+            background: "salmon",
+            height: "100%",
+            marginTop: "0px",
+            padding: "30px",
+          }}
+        >
+          ENTER <bold>NAME</bold> N <bold>AGE</bold>
+        </h1>
+
         <Persons />
       </div>
     );
--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -20,18 +20,42 @@ class AddPerson extends Component {
     return (
       <div className="AddPerson">
         <input
+          style={{
+            textAlign: "center",
+            border: "1px solid green",
+            boxSizing: "border-box",
+            boxShadow: "0 2px 2px #ccc",
+            margin: "20px",
+            textDecoration: "bold",
+          }}
           type="text"
           placeholder="Name"
           onChange={this.nameChangeHandler}
           value={this.state.name}
         />
         <input
+          style={{
+            textAlign: "center",
+            border: "1px solid green",
+            boxSizing: "border-box",
+            boxShadow: "0 2px 2px #ccc",
+            margin: "20px",
+            textDecoration: "bold",
+          }}
           type="Number"
           placeholder="Age"
           onChange={this.ageChangeHandler}
           value={this.state.age}
         />
         <button
+          style={{
+            textAlign: "center",
+            border: "1px solid yellow",
+            boxSizing: "border-box",
+            boxShadow: "0 2px 2px #ccc",
+            margin: "20px",
+            textDecoration: "bold",
+          }}
           onClick={() =>
             this.props.personAdded(this.state.name, this.state.age)
           }
