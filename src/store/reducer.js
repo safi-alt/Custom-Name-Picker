bb130f3ba54942c2036c7167cb5babb680f771db
safi <safiazim8@gmail.com>
2020-07-10 23:11:19 +0500

    Creating Reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,28 +1,17 @@
-import * as actionTypes from './actions';
+import * as actionTypes from "./actions";
 
 const initialState = {
-    persons: []
+  persons: [],
 };
 
-const reducer = ( state = initialState, action ) => {
-    switch ( action.type ) {
-        case actionTypes.ADD_PERSON:
-            const newPerson = {
-                id: Math.random(), // not really unique but good enough here!
-                name: 'Max',
-                age: Math.floor( Math.random() * 40 )
-            }
-            return {
-                ...state,
-                persons: state.persons.concat( newPerson )
-            }
-        case actionTypes.REMOVE_PERSON:
-            return {
-                ...state,
-                persons: state.persons.filter(person => person.id !== action.personId)
-            }
-    }
-    return state;
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_PERSON:
+      return {};
+    case actionTypes.REMOVE_PERSON:
+      return {};
+  }
+  return state;
 };
 
 export default reducer;
