27ba920f3b4ccaca36fabdf3f2c083788fa95649
safi <safiazim8@gmail.com>
2020-07-10 20:46:09 +0500

    Completed app structure

---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+    persons: []
+};
+
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
+        case actionTypes.ADD_PERSON:
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: 'Max',
+                age: Math.floor( Math.random() * 40 )
+            }
+            return {
+                ...state,
+                persons: state.persons.concat( newPerson )
+            }
+        case actionTypes.REMOVE_PERSON:
+            return {
+                ...state,
+                persons: state.persons.filter(person => person.id !== action.personId)
+            }
+    }
+    return state;
+};
+
+export default reducer;
